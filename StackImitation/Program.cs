56135d5067cb32e4a6b4f9163a64cfeb90c43611
redemaxlukyan <redemaxlukyan@i.ua>
2023-04-03 09:06:31 +0300

    first commit

---
+++ StackImitation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImitation
+{
+    class StackImitation
+    {
+        private int[] stack;
+        private int topIndex;
+
+        public StackImitation(int size)
+        {
+            stack = new int[size];
+            topIndex = 0;
+        }
+
+        public void SetNum(int num)
+        {
+            if (topIndex == stack.Length)
+            {
+                Console.WriteLine("Стек заповнено.");
+                return;
+            }
+
+            stack[topIndex] = num;
+            topIndex++;
+        }
+
+        public int GetNum()
+        {
+            if (topIndex == 0)
+            {
+                Console.WriteLine("Стек порожній.");
+                return 0;
+            }
+
+            topIndex--;
+            return stack[topIndex];
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
